0ed945ef308757f7773e455a60d795d67931446c
khewdev <khewdev@gmail.com>
2021-08-02 01:15:21 +0800

    fixed uuid issue, need to clear other uuid packages, currently using react-native-uuid

--- App.js
+++ App.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text } from 'react-native';
+import uuid from 'react-native-uuid';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {
+      id: uuid.v4(),
+      text: 'Milk'
+    },
+    {
+      id: uuid.v4(),
+      text: 'Eggs'
+    },
+    {
+      id: uuid.v4(),
+      text: 'Bread'
+    },
+    {
+      id: uuid.v4(),
+      text: 'Juice'
+    },
+  ]);
+
   return (
     <View>
       <Text>Hello World</Text>
